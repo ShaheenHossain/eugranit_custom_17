e3b12f80cd88b5ad36884fc671beb20993f1000c
Md. Shaheen Hossain <rapidgrps@princegroup-bd.com>
2025-02-21 16:36:36 +0600

    update invoice date

--- models/sale_order.py
+++ models/sale_order.py
@@ -14,29 +14,41 @@ class SaleOrder(models.Model):
         })
         return invoice_vals
 
+
+
+
+# class AccountMove(models.Model):
+#     _inherit = 'account.move'
+#
+#
+#     commitment_delivery_date = fields.Date(
+#         string="Delivery Date",
+#         related="invoice_origin_id.commitment_delivery_date",
+#         store=True
+#     )
+#     measurement_date = fields.Date(
+#         string="Aufmaß Datum",
+#         related="invoice_origin_id.measurement_date",
+#         store=True
+#     )
+#
+#     invoice_origin_id = fields.Many2one(
+#         'sale.order',
+#         compute="_compute_invoice_origin_id",
+#         store=True,
+#         string="Original Quotation"
+#     )
+#
+#     def _compute_invoice_origin_id(self):
+#         for move in self:
+#             move.invoice_origin_id = self.env['sale.order'].search(
+#                 [('name', '=', move.invoice_origin)], limit=1
+#             ) or False
+
+
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    commitment_delivery_date = fields.Date(
-        string="Delivery Date",
-        related="invoice_origin_id.commitment_delivery_date",
-        store=True
-    )
-    measurement_date = fields.Date(
-        string="Aufmaß Datum",
-        related="invoice_origin_id.measurement_date",
-        store=True
-    )
+    commitment_delivery_date = fields.Date(string="Delivery Date")
+    measurement_date = fields.Date(string="Aufmaß Datum")
 
-    invoice_origin_id = fields.Many2one(
-        'sale.order',
-        compute="_compute_invoice_origin_id",
-        store=True,
-        string="Original Quotation"
-    )
-
-    def _compute_invoice_origin_id(self):
-        for move in self:
-            move.invoice_origin_id = self.env['sale.order'].search(
-                [('name', '=', move.invoice_origin)], limit=1
-            ) or False
